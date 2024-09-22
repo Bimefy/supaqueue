095df6b92c4f472fdd5d0a011d3366a6210438f0
Jo <jo@b-fine.be>
2024-09-22 06:47:53 +0200

    update supabase package + update supabase server

--- scripts/getPgClient.ts
+++ scripts/getPgClient.ts
@@ -1,7 +1,7 @@
 import pg from "pg";
 export const getPgClient = () => {
   return new pg.Client({
-    host: "aws-0-ca-central-1.pooler.supabase.com",
+    host: "aws-0-eu-central-1.pooler.supabase.com",
     port: 5432,
     database: "postgres",
     user: `postgres.${process.env.PROJECT_REF}`,
